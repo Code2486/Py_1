5eea9fa467cb9a7117484d519754224befcbc0ed
Code2486 <smm813sa@gmail.com>
2026-02-27 14:44:26 +0330

    4.py

---
+++ 4.py
@@ -0,0 +1,24 @@
+#دریافت سه عدد و تشخیص بزرگ ترین و کوچک ترین آنها 
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+if a > b:
+    max_num = a
+if b > a:
+    max_num = b
+if c > b:
+    max_num = c
+    print("The largest number is:", max_num)
+
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+if a < b:
+    min_num = a
+if b < a:
+    min_num = b
+if c < b:
+    min_num = c
+    print("The largest number is:", min_num)
