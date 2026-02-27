2f05593e6f8a74f3c93df17cd74486b140e31e37
Code2486 <smm813sa@gmail.com>
2026-02-27 14:29:01 +0330

    1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,8 @@
+#دریافت عددی از ورودی و تشخیص اینکه مثبت است یا منفی
+
 num = float(input("Enter a number: "))
 if num > 0:
     print("The number is positive")
 else:
+
     print("The number is negative")
